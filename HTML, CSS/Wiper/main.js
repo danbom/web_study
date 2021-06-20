db14ad75c5e95ba04749b5fce786f8272728eace
danbom <bear0369@naver.com>
2021-06-21 00:58:59 +0900

    Vector2에 연산기능들 추가

--- HTML, CSS/Wiper/main.js
+++ HTML, CSS/Wiper/main.js
@@ -26,6 +26,36 @@ class Vector2{
     divS(s){
         return new Vector2(this.x / s, this.y / s);
     }
+
+    addV(v){
+        return new Vector2(this.x + v.x, this.y + v.y);
+    }
+
+    subV(v){
+        return new Vector2(this.x - v.x, this.y - v.y);
+    }
+
+    mulV(v){
+        return new Vector2(this.x * v.x, this.y * v.y);
+    }
+
+    divV(v){
+        return new Vector2(this.x / v.x, this.y / v.y);
+    }
+
+    len(){
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    normalized(){
+        const len = this.len();
+
+        return new Vector2(this.x / len, this.y / len);
+    }
+
+    dot(v){
+        return this.x * v.x + this.y * v.y;
+    }
 }
 
 class Game {
