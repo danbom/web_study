21265803bc067bed076dbfe29adcbc7fec138bb4
danbom <52441697+danbom@users.noreply.github.com>
2021-06-22 22:18:14 +0900

    friction 추가 및 update() 수정

--- HTML, CSS/Wiper/main.js
+++ HTML, CSS/Wiper/main.js
@@ -1,157 +1,189 @@
 // alert("test")
 
+"use strict";
 
+class Vector2 {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-'use strict'
+  addS(s) {
+    return new Vector2(this.x + s, this.y + s);
+  }
 
-class Vector2{
+  subS(s) {
+    return new Vector2(this.x - s, this.y - s);
+  }
 
-    constructor(x, y){
-        this.x = x;
-        this.y = y;
-    }
+  mulS(s) {
+    return new Vector2(this.x * s, this.y * s);
+  }
 
-    addS(s){
-        return new Vector2(this.x + s, this.y + s);
-    }
+  divS(s) {
+    return new Vector2(this.x / s, this.y / s);
+  }
 
-    subS(s){
-        return new Vector2(this.x - s, this.y - s);
-    }
+  addV(v) {
+    return new Vector2(this.x + v.x, this.y + v.y);
+  }
 
-    mulS(s){
-        return new Vector2(this.x * s, this.y * s);
-    }
+  subV(v) {
+    return new Vector2(this.x - v.x, this.y - v.y);
+  }
 
-    divS(s){
-        return new Vector2(this.x / s, this.y / s);
-    }
+  mulV(v) {
+    return new Vector2(this.x * v.x, this.y * v.y);
+  }
 
-    addV(v){
-        return new Vector2(this.x + v.x, this.y + v.y);
-    }
+  divV(v) {
+    return new Vector2(this.x / v.x, this.y / v.y);
+  }
 
-    subV(v){
-        return new Vector2(this.x - v.x, this.y - v.y);
-    }
+  len() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
 
-    mulV(v){
-        return new Vector2(this.x * v.x, this.y * v.y);
-    }
+  normalized() {
+    const len = this.len();
 
-    divV(v){
-        return new Vector2(this.x / v.x, this.y / v.y);
-    }
+    return new Vector2(this.x / len, this.y / len);
+  }
 
-    len(){
-        return Math.sqrt(this.x * this.x + this.y * this.y);
-    }
-
-    normalized(){
-        const len = this.len();
-
-        return new Vector2(this.x / len, this.y / len);
-    }
-
-    dot(v){
-        return this.x * v.x + this.y * v.y;
-    }
+  dot(v) {
+    return this.x * v.x + this.y * v.y;
+  }
 }
 
-class Ball{
-    constructor(pos, r){
-        this.pos = pos;
-        this.r = r;
-        this.mass = r;
-        this.v = new Vector2(0,0);
-    }
+class Ball {
+  constructor(pos, r) {
+    this.pos = pos;
+    this.r = r;
+    this.mass = r;
+    this.v = new Vector2(0, 0);
+  }
 
-    addForce(f){
-        this.v = this.v.addS(f.divS(this.mass))
-    }
+  addForce(f) {
+    this.v = this.v.addV(f.divS(this.mass));
+  }
 
-    update(){
-        this.pos = this.pos.addV(this.v);
-    }
+  update(friction) {
+    this.pos = this.pos.addV(this.v);
+    this.v = this.v.mulS(friction);
+  }
 
-    render(ctx){
-        ctx.beginPath();
-        ctx.arc(this.pos.x, this.pos.y, this.r, 0, 2 * Math.PI);
-        ctx.stroke();
-    }
+  render(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.pos.x, this.pos.y, this.r, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
 }
 
 class Game {
-    constructor(width, height){
-        this.width = width;
-        this.height = height;
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
 
-        this.cvs = document.getElementById("cvs");
-        this.cvs.setAttribute("width", width);
-        this.cvs.setAttribute("height", height);
-        this.ctx = this.cvs.getContext("2d");
+    this.cvs = document.getElementById("cvs");
+    this.cvs.setAttribute("width", width);
+    this.cvs.setAttribute("height", height);
+    this.ctx = this.cvs.getContext("2d");
 
-        this.times = [];
+    this.times = [];
+  }
+
+  start() {
+    this.init();
+    this.run();
+  }
+
+  init() {
+    this.friction = 0.995;
+
+    this.balls = [];
+    const spread = 0.8;
+
+    for (let i = 0; i < 100; i++) {
+      let b = new Ball(
+        new Vector2(
+          (Math.random() - 0.5) * this.width * spread + this.width / 2.0,
+          (Math.random() - 0.5) * this.height * spread + this.height / 2.0
+        ),
+        Math.random() * 10 + 15
+      );
+
+      b.addForce(
+        new Vector2(Math.random() - 0.5, Math.random() - 0.5).mulV(
+          new Vector2(100, 100)
+        )
+      );
+      this.balls.push(b);
     }
+  }
 
-    start(){
-        this.init();
-        this.run();
+  run() {
+    // console.log(true);
+    const now = performance.now();
+    this.times.push(now);
+
+    if (now - this.times[0] > 1000) {
+      this.times.shift();
     }
+    // console.log(this.times.length);
 
-    init(){
+    this.update();
+    this.render();
 
-        this.balls = [];
+    requestAnimationFrame(this.run.bind(this));
+  }
 
-        const spread = 0.8;
+  update() {
+    this.balls.forEach(b => {
+      b.update(this.friction);
+    });
 
-        for (let i = 0; i < 100; i++)
-        {
-            let b = new Ball(new Vector2((Math.random() - 0.5) * this.width * spread + this.width / 2.0, (Math.random() - 0.5) * this.height * spread + this.height / 2.0), Math.random() * 10 + 15);
+    this.balls.forEach(b => {
+      if (b.pos.x - b.r < 0) {
+        b.pos.x = b.r;
+        b.v.x = b.v.x * -1;
+      }
+    });
 
-            b.addForce(new Vector2((Math.random() - 0.5), (Math.random() - 0.5)).mulV(new Vector2(100, 100)));
-            this.balls.push(b);
-        }
+    this.balls.forEach(b => {
+      if (b.pos.y - b.r < 0) {
+        b.pos.y = b.r;
+        b.v.y = b.v.y * -1;
+      }
+    });
 
-        
-        
-    }
+    this.balls.forEach(b => {
+      if (b.pos.x + b.r >= this.width) {
+        b.pos.x = this.width - b.r;
+        b.v.x = b.v.x * -1;
+      }
+    });
 
-    run(){
-        // console.log(true);
-        const now = performance.now();
-        this.times.push(now);
+    this.balls.forEach(b => {
+      if (b.pos.y + b.r >= this.height) {
+        b.pos.y = this.height - b.r;
+        b.v.y = b.v.y * -1;
+      }
+    });
+  }
 
-        if (now - this.times[0] > 1000) {
-            this.times.shift();
-        }
-        // console.log(this.times.length);
+  render() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
 
-        this.update();
-        this.render();
+    this.ctx.fillStyle = "white";
+    this.ctx.font = "22px Quicksand";
+    this.ctx.fillText(this.times.length - 1 + "fps", 15, 35);
 
-        requestAnimationFrame(this.run.bind(this));
-    }
-
-    update(){
-        this.balls.forEach(b => {
-            b.update();
-        });
-    }
-
-    render(){
-        this.ctx.clearRect(0, 0, this.width, this.height);
-
-        this.ctx.fillStyle = "white"
-        this.ctx.font = '22px Quicksand';
-        this.ctx.fillText(this.times.length - 1 + "fps", 15, 35);
-
-        this.balls.forEach(b => {
-            b.render(this.ctx);
-        });
-    }
+    this.balls.forEach(b => {
+      b.render(this.ctx);
+    });
+  }
 }
 
 window.onload = () => {
-    new Game(1600,900).start();
-}
+  new Game(1600, 900).start();
+};
